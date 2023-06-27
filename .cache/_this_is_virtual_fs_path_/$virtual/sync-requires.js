4eefc13861d73096906fdee9cc3b097759e2fb05
Lucas Gouvêa <lucas.fim@gmail.com>
2023-06-27 01:07:30 -0300

    gatsby-template: Template import

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/k2/Projects/guitar-compendium/.cache/dev-404-page.js")),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": preferDefault(require("/home/k2/Projects/guitar-compendium/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")),
+  "component---src-pages-404-tsx": preferDefault(require("/home/k2/Projects/guitar-compendium/src/pages/404.tsx"))
+}
+
